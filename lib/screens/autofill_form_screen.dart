ac27fbe76f0c9f441c67ba8f45d6db0962c8cc6b
prince <princesuke@gmail.com>
2025-07-01 13:46:35 +0700

    feat: add logic view

--- lib/screens/autofill_form_screen.dart
+++ lib/screens/autofill_form_screen.dart
@@ -8,6 +8,11 @@ class AutofillFormScreen extends StatefulWidget {
 }
 
 class _AutofillFormScreenState extends State<AutofillFormScreen> {
+  final idController = TextEditingController();
+  final nameController = TextEditingController();
+  final surnameController = TextEditingController();
+  final expirycontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +22,42 @@ class _AutofillFormScreenState extends State<AutofillFormScreen> {
         child: Column(
           children: [
             TextField(
+              controller: idController,
               decoration: const InputDecoration(labelText: 'เลขบัตรประชาชน'),
             ),
-            TextField(decoration: const InputDecoration(labelText: 'ชื่อ')),
-            TextField(decoration: const InputDecoration(labelText: 'นามสกุล')),
             TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: 'ชื่อ'),
+            ),
+            TextField(
+              controller: surnameController,
+              decoration: const InputDecoration(labelText: 'นามสกุล'),
+            ),
+            TextField(
+              controller: expirycontroller,
               decoration: const InputDecoration(labelText: 'ปีหมดอายุ'),
             ),
             const SizedBox(height: 24),
-            ElevatedButton(onPressed: () {}, child: const Text('ยืนยัน')),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder:
+                      (c) => AlertDialog(
+                        content: const Text('บันทึกสำเร็จ'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.popUntil(context, (r) => r.isFirst);
+                            },
+                            child: const Text('กลับหน้าแรก'),
+                          ),
+                        ],
+                      ),
+                );
+              },
+              child: const Text('ยืนยัน'),
+            ),
           ],
         ),
       ),
